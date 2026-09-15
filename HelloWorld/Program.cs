419f3678b3982a95ea034fe7d5a8d083cf5bd520
Zackery Racz <zrracz@gmail.com>
2026-09-14 20:08:02 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Zackery Racz
+* Course: CSCI 1250, section 201
+* Assignment: Lab ), Hello World
+* Date: September 14, 2026
+* Description: prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Zackery Racz.");
+Console.WriteLine("This is my first program at ETSU.");
